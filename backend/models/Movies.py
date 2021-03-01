224ba3918ccf3124e83824380a15fec772786b74
Hernan <hgonzalez@tiendamia.com>
2021-02-28 23:31:26 -0300

    Improving the Movies model

--- backend/models/Movies.py
+++ backend/models/Movies.py
@@ -4,30 +4,26 @@ class Movies(Omdb):
     
 
     def __init__(self):
-        self.imdb_id     = ''
-        self.title       = ''
-        self.year        = ''
-        self.released    = ''
-        self.actors      = ''
-        self.awards      = ''
-        self.box_office  = ''
-        self..director   = ''
-        self.genre       = ''
-        self.language    = ''
-        self.metascore   = ''
-        self.plot        = ''
-        self.poster      = ''
-        self.rated       = ''
-        self.response    = ''
-        self.runtime     = ''
-        self.writer      = ''
-        self.imdb_rating = ''
+        self.movie_data: dict  = {}
+
+
+    def set_movie_data(self, movie_data: dict) -> None:
+        self.movie_data = movie_data
+
+    
+    def get_movie_data(self) -> dict:
+        return self.movie_data
+
+
+    def set_data(self, key: str, value: str) -> None:
+        self.movie_data[key] = value
+
+    
+    def get_data(self, key) -> dict:
+        return self.movie_data[key]
 
 
     def get_movie_or_show(self, imdb_id):
-        # TODO: Hacer un setter que procese y setee la data pertinente
         return self.get_by_imdb_id(self, imdb_id)
 
 
-    def set_data(self, key, value):
-        self.key = value
